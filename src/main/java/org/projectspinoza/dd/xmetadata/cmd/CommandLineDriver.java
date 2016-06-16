bd8e61f42fcbe9d435d25b351a7828f488d7d076
Muhammad Sadiq <m.sadiq.khan92@gmail.com>
2016-06-16 15:37:57 +0500

    minor modification

--- src/main/java/org/projectspinoza/dd/xmetadata/cmd/CommandLineDriver.java
+++ src/main/java/org/projectspinoza/dd/xmetadata/cmd/CommandLineDriver.java
@@ -5,27 +5,33 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.projectspinoza.dd.xmetadata.XmetaApi;
 
 import com.beust.jcommander.JCommander;
 
 public class CommandLineDriver {
+  private static Logger LOG = LogManager.getRootLogger();
   private static XmetaApi apiHandler = null;
   
   public static void main(String[] args) {
     CommandLineDriver clitest = new CommandLineDriver();
     Scanner scanner = new Scanner(new InputStreamReader(System.in));
-    System.out.println("WELCOME TO XMETA[MD EXTRACTION]");
+    LOG.info("WELCOME TO XMETA[MD EXTRACTION]");
+    LOG.info("use connect command to connect to the database");
     while(true){
       System.out.print("xmeta>");
       String cmd = scanner.nextLine();
       if(clitest.validated(cmd)){
         if(cmd.equalsIgnoreCase("exit")){
+          apiHandler.closeConnection();
+          apiHandler = null;
           System.exit(0);
         }
         clitest.exec(cmd.split(" "));
       }else{
-        System.out.println("for help type: xmeta --help OR xmeta <commandName> --help");
+        LOG.info("for help type: xmeta --help OR xmeta <commandName> --help");
       }
     }
   }
@@ -51,7 +57,7 @@ public class CommandLineDriver {
     try {
       exec(rootCommander, parsedCommand);
     } catch (Exception e){
-      e.printStackTrace();
+      LOG.error("make sure you've entered a valid command");
     }
     
   }
@@ -74,7 +80,6 @@ public class CommandLineDriver {
       for(String db : dbs){
         System.out.print(db + ", ");
       }
-      System.out.println();
       break;
     case showTables:
       CommandShowTables cmdlisttbl = (CommandShowTables) rootCommander.getCommands().get("xmeta").getCommands().get(parsedCommand).getObjects().get(0);
@@ -82,7 +87,6 @@ public class CommandLineDriver {
       for(String tbl : tbls){
         System.out.print(tbl + ", ");
       }
-      System.out.println();
       break;
     case showColumns:
       CommandShowColumns cmdlistCol = (CommandShowColumns) rootCommander.getCommands().get("xmeta").getCommands().get(parsedCommand).getObjects().get(0);
@@ -97,7 +101,6 @@ public class CommandLineDriver {
       for(String key : colsWithType.keySet()){
         System.out.print(key + ":" + colsWithType.get(key) + ", ");
       }
-      System.out.println();
       break;
     case getColLength:
       CommandGetColLength cmdColLen = (CommandGetColLength) rootCommander.getCommands().get("xmeta").getCommands().get(parsedCommand).getObjects().get(0);
@@ -112,27 +115,26 @@ public class CommandLineDriver {
       for(String key : colType.keySet()){
         System.out.print(key + ":" + colType.get(key) + ", ");
       }
-      System.out.println();
       break;
     case getColAtPos:
       CommandGetColAtPos cmdGetColAtPos = (CommandGetColAtPos) rootCommander.getCommands().get("xmeta").getCommands().get(parsedCommand).getObjects().get(0);
       List<String> getColAtPos = cmdGetColAtPos.exec(apiHandler);
       for(String key : getColAtPos){
-        System.out.println(key + ", ");
+        System.out.print(key + ", ");
       }
       break;
     case getPK:
       CommandGetPK cmdGetPK = (CommandGetPK) rootCommander.getCommands().get("xmeta").getCommands().get(parsedCommand).getObjects().get(0);
       List<String> getPK = cmdGetPK.exec(apiHandler);
       for(String key : getPK){
-        System.out.println(key + ", ");
+        System.out.print(key + ", ");
       }
       break;
     case getFK:
       CommandGetFK cmdGetFK = (CommandGetFK) rootCommander.getCommands().get("xmeta").getCommands().get(parsedCommand).getObjects().get(0);
       List<String> getFK = cmdGetFK.exec(apiHandler);
       for(String key : getFK){
-        System.out.println(key + ", ");
+        System.out.print(key + ", ");
       }
       break;
     case getFKRefTables:
@@ -141,13 +143,12 @@ public class CommandLineDriver {
       for(String key : GetFKRefTables.keySet()){
         System.out.print(key + ":" + GetFKRefTables.get(key) + ", ");
       }
-      System.out.println();
       break;
     case getIndexes:
       CommandGetIndexes cmdGetindexes = (CommandGetIndexes) rootCommander.getCommands().get("xmeta").getCommands().get(parsedCommand).getObjects().get(0);
       List<String> getindexes = cmdGetindexes.exec(apiHandler);
       for(String key : getindexes){
-        System.out.println(key + ", ");
+        System.out.print(key + ", ");
       }
       break;
     case getAllIdxRefTable:
@@ -156,7 +157,6 @@ public class CommandLineDriver {
       for(String key : GetAllIdxRefTable.keySet()){
         System.out.print(key + ":" + GetAllIdxRefTable.get(key) + ", ");
       }
-      System.out.println();
       break;
     case existsDatabase:
       CommandExistsDatabase existsDb = (CommandExistsDatabase) rootCommander.getCommands().get("xmeta").getCommands().get(parsedCommand).getObjects().get(0);
@@ -175,6 +175,8 @@ public class CommandLineDriver {
     default:
       break;
     }
+    
+    System.out.println();
   }
   private void registerCommands(JCommander rootCommander){
     rootCommander.addCommand("xmeta", new CommandXmeta());
